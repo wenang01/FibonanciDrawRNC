0b6dc6e08af694e4886dec71ee24d8af0a637367
wenangbackup <wenangbackup@gmail.com>
2021-05-01 09:46:33 +0700

    need to clear screen before set state

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import DissLike from './component/DissLike'
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-        icon : require("./component/assets/like.png"),
-        icon2 : require("./component/assets/disslike.png")
+      iconSetLike: require("./component/assets/like.png"),
+      iconSetDissLike: require("./component/assets/disslike.png")
     }
   }
-  
+
   test = []
   panjang = 10
   lebar = 10
@@ -21,89 +21,44 @@ class App extends Component {
   y = 0
 
   changeIcon = () => {
-    this.setState({icon : require("./component/assets/disslike.png")})
-    // let test = []
-    // const panjang = 10
-    // const lebar = 10
-    // let count = 0
-    // let x = 0
-    // let y = 0
-    // for(x=1; x<=panjang; x++) {
-		// 	for(y=1; y<=lebar; y++) {
-		// 		count++;
-		// 		let countTemp = 0;
-		// 		for(let z = 1; z <= count; z++) {
-		// 			if(count % z == 0) {
-		// 				countTemp++;
-		// 			}
-		// 		}
-		// 		if(countTemp == 2) {
-		// 			this.test.push(<TouchableOpacity onPress={this}>
-    //         <DissLike/>
-    //       </TouchableOpacity>);
-		// 		}/*
-    //     else if(countTemp != 2) {
-		// 			this.test.push(<TouchableOpacity onPress={this.changeIcon}>
-    //         <Like/>
-    //       </TouchableOpacity>);
-		// 		}*/
-    //     else{
-    //       this.test.push(<TouchableOpacity onPress={this}>
-    //         <Like/>
-    //       </TouchableOpacity>);
-    //     }
-		// 	}
-    // }
+    this.setState({ iconSetLike: require("./component/assets/disslike.png") })
   }
 
   render() {
-    // const ruang = this.panjang * this.lebar
-    // for(let z = 0; z <= ruang; z++) {
-    //   this.test.push(<TouchableOpacity onPress={this.changeIcon}>
-    //     <Like/>
-    //   </TouchableOpacity>);
-    // }
-    // let test = []
     const panjang = 10
     const lebar = 10
     let count = 0
     let x = 0
     let y = 0
-    for(x=1; x<=panjang; x++) {
-			for(y=1; y<=lebar; y++) {
-				count++;
-				let countTemp = 0;
-				for(let z = 1; z <= count; z++) {
-					if(count % z == 0) {
-						countTemp++;
-					}
-				}
-				if(countTemp == 2) {
-					this.test.push(<TouchableOpacity onPress= {this.changeIcon}>
-            <DissLike icon = {this.state.icon} />
+    for (x = 1; x <= panjang; x++) {
+      for (y = 1; y <= lebar; y++) {
+        count++;
+        let countTemp = 0;
+        for (let z = 1; z <= count; z++) {
+          if (count % z == 0) {
+            countTemp++;
+          }
+        }
+        if (countTemp == 2) {
+          this.test.push(<TouchableOpacity onPress={
+            () => {
+              this.test.
+                this.setState({ iconSetLike: require("./component/assets/disslike.png") })
+            }
+          }>
+            <DissLike icon={this.state.iconSetLike} />
           </TouchableOpacity>);
-				}/*
-        else if(countTemp != 2) {
-					this.test.push(<TouchableOpacity onPress={this.changeIcon}>
-            <Like/>
-          </TouchableOpacity>);
-				}*/
-        else{
-          this.test.push(<TouchableOpacity onPress={}>
-            <Like/>
+        } else {
+          this.test.push(<TouchableOpacity>
+            <Like />
           </TouchableOpacity>);
         }
-			}
+      }
     }
     return (
-      <View>
-        <ScrollView>
-          <Text>{this.test}</Text>
-        </ScrollView>
-        {/* <TouchableOpacity onPress={this.changeIcon}>
-          <Like icon = {this.state.icon} />
-        </TouchableOpacity> */}
-      </View>
+      <ScrollView>
+        <Text>{this.test}</Text>
+      </ScrollView>
     );
   }
 }
